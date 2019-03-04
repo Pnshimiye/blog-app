f8d82924dd8a391ee7f3543be36017cfb14c048c
Pnshimiye <pnshimiye@gmail.com>
2019-03-04 18:24:34 +0200

    change Database URI

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ class Config:
 
     QUOTES_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json' 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pauline:0788812609@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
